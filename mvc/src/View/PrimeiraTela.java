cf19f349c8e3143e87516fe20483d9766d2356fb
Diego Silva Santos Rocha <TER04973@rootbrasil.intranet>
2019-01-26 14:12:42 -0200

    Java com jframe

--- mvc/src/View/PrimeiraTela.java
+++ mvc/src/View/PrimeiraTela.java
@@ -3,10 +3,19 @@ package View;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class PrimeiraTela {
 
 	private JFrame frame;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +47,26 @@ public class PrimeiraTela {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(108, 112, 205, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton btnMostrar = new JButton("Mostrar");
+		btnMostrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0){
+				
+				JOptionPane.showMessageDialog(null,"Nome:"+textField.getText());
+				
+			}
+		});
+		btnMostrar.setBounds(163, 153, 107, 23);
+		frame.getContentPane().add(btnMostrar);
+		
+		JLabel lblNome = new JLabel("NOME");
+		lblNome.setBounds(108, 84, 46, 14);
+		frame.getContentPane().add(lblNome);
 	}
-
 }
