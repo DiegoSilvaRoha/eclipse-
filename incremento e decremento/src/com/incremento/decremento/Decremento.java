b5ca3e39fecbccaad64be81bd6d55ad8908e18f6
Diego Silva Santos Rocha <TER04973@rootbrasil.intranet>
2019-01-27 14:58:46 -0200

    incremento e decremento

---
+++ incremento e decremento/src/com/incremento/decremento/Decremento.java
@@ -0,0 +1,11 @@
+package com.incremento.decremento;
+
+public class Decremento {
+
+	public static void main (String args []) {
+		int numero = 1;
+		numero--;
+		System.out.println(numero--);
+
+	}
+}
---
+++ incremento e decremento/src/com/incremento/decremento/Incremento.java
@@ -0,0 +1,11 @@
+package com.incremento.decremento;
+
+public class Incremento {
+
+	public static void main (String args[]) {
+		int numero = 1 ;
+		numero++;
+		//numero++ ou ++numero
+		System.out.println(numero++);
+	}
+}
