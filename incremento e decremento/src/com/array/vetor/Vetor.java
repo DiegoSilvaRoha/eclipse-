4fa9ea4df9fa3530781f54c5ccbb8daf0453a26a
Diego Silva Santos Rocha <TER04973@rootbrasil.intranet>
2019-01-27 19:09:38 -0200

    vetor

---
+++ incremento e decremento/src/com/array/vetor/Vetor.java
@@ -0,0 +1,14 @@
+package com.array.vetor;
+
+public class Vetor {
+	public static void main (String args[]) {
+		//posições: 0,1 e 2
+		String [] nomes = new String[3];
+		nomes[0] = "Carlos";
+		nomes[1] = "Diego";
+		nomes[2] = "Danilo";
+
+		System.out.println(nomes[1]);;
+	}
+
+}
---
+++ incremento e decremento/src/com/array/vetor/Vetor2.java
@@ -0,0 +1,14 @@
+package com.array.vetor;
+
+public class Vetor2 {
+
+	public static void main (String args[]) {
+		
+		String[] nomes = {"Diego","Danilo","Carlos"};
+		//ou sobrescrever nomes na posicao do array
+		//nomes [0] = "EU";
+		System.out.println(nomes[0]);
+		
+
+	}
+}
